811bfa83d57953fc735a3060cf0c4e94425818cd
xiawuhao2013 <xiawuhao2011@foxmail.com>
2020-01-07 10:16:38 +0800

    release: v0.0.1

---
+++ typedefVsDefine/include/customize.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#define INTPTR_3 int*
+
+namespace Customize
+{
+    typedef int* INTPTR_2;
+
+}   // Customize
+
---
+++ typedefVsDefine/typedefVsDefine.cpp
@@ -0,0 +1,31 @@
+﻿/*
+* typedef有自己的作用域。
+* #define文件内可见，namespace对它不起作用。
+*/
+
+#include"./include/customize.h";
+
+#include<iostream>
+
+namespace Idle
+{
+#define INTPTR_1 int*
+
+}   // Idle
+
+namespace Customize
+{
+    int main()
+    {
+        //
+        INTPTR_1 p1, p2;    // >p1 int*     >p2 int
+        INTPTR_2 p3, p4;    // >p3 int*     >p4 int*
+        INTPTR_3 p;
+
+        //
+        const INTPTR_1 p5 = nullptr;    // >p5 const int*
+        const INTPTR_2 p6 = nullptr;    // >p6 int* const
+        INTPTR_1 const p7 = nullptr;    // >p7 int* const;
+        INTPTR_2 const p8 = nullptr;    // >p8 int* const;
+    }
+}   // Customize
